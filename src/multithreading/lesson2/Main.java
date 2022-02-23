d6d1f46664a46c9020fda3290789b807566cfc23
DaNaRim <rangar.danarim@gmail.com>
2022-02-23 23:18:10 +0200

    multithreading lesson2

---
+++ src/multithreading/lesson2/Main.java
@@ -0,0 +1,21 @@
+package multithreading.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread myThread = new MyThread();
+        myThread.start();
+//        myThread.setPriority(Thread.MAX_PRIORITY);
+        Thread.yield();
+
+        myThread.join();
+
+//        Thread.sleep(1000);
+
+        System.out.println("Tread main");
+    }
+
+
+}
