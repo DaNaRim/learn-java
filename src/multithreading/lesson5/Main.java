2975b4276f46e8b44682bbbfa7cdaef703d2d1a3
DaNaRim <rangar.danarim@gmail.com>
2022-02-24 00:47:48 +0200

    multithreading lesson5

---
+++ src/multithreading/lesson5/Main.java
@@ -0,0 +1,41 @@
+package multithreading.lesson5;
+
+public class Main {
+    volatile static int i; //no cash
+
+    public static void main(String[] args) {
+        new MyThreadRead().start();
+        new MyThreadWrite().start();
+    }
+
+    static class MyThreadWrite extends Thread {
+
+        @Override
+        public void run() {
+            while (i < 5) {
+                System.out.println("increment i to " + ++i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class MyThreadRead extends Thread {
+
+        @Override
+        public void run() {
+            int localVar = i;
+            while (localVar < 5) {
+                if (localVar != i) {
+                    System.out.println("new value of i is " + i);
+                    localVar = i;
+                }
+            }
+        }
+    }
+}
+
+
