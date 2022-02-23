ded49b952f67bb6dc8f395fffb9653a0ffd329ce
DaNaRim <rangar.danarim@gmail.com>
2022-02-24 00:47:54 +0200

    multithreading lesson6

---
+++ src/multithreading/lesson6/Main.java
@@ -0,0 +1,23 @@
+package multithreading.lesson6;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    static AtomicInteger i = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int j = 0; j < 10_000; j++) {
+            new MyThread().start();
+        }
+        Thread.sleep(2_000);
+        System.out.println(i);
+    }
+
+    static class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            i.incrementAndGet();
+        }
+    }
+}
