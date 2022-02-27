6151ce8c084f4a7d15c12a6f55278ae15e8506e2
DaNaRim <rangar.danarim@gmail.com>
2022-02-27 16:15:34 +0200

    multithreading lesson11

---
+++ src/multithreading/lesson11/Main.java
@@ -0,0 +1,24 @@
+package multithreading.lesson11;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Resource resource = new Resource(5, 5);
+
+        MyThread myThread = new MyThread(resource);
+        myThread.setName("one");
+        MyThread myThread2 = new MyThread(resource);
+
+        myThread.start();
+        myThread2.start();
+
+        myThread.join();
+        myThread2.join();
+
+        System.out.println(resource.getI());
+        System.out.println(resource.getJ());
+    }
+
+
+}
