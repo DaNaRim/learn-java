6cabf4459f3c4e4476a778cb8098dd0c471b2bfa
DaNaRim <rangar.danarim@gmail.com>
2022-02-28 22:52:09 +0200

    multithreading lesson15

---
+++ src/multithreading/lesson15/Main.java
@@ -0,0 +1,36 @@
+package multithreading.lesson15;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(new MyRunnable());
+        System.out.println(executorService.submit(new MyCallable()).get());
+        executorService.shutdown();
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(1);
+        }
+    }
+
+
+    static class MyCallable implements Callable<String> {
+
+        @Override
+        public String call() {
+            return "2";
+        }
+    }
+}
+
