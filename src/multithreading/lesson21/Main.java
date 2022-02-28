8a94d4259edf0994231a35525f9bdb4b4b0b47fa
DaNaRim <rangar.danarim@gmail.com>
2022-02-28 23:00:36 +0200

    multithreading lesson21

---
+++ src/multithreading/lesson21/Main.java
@@ -0,0 +1,30 @@
+package multithreading.lesson21;
+
+import java.util.concurrent.Phaser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Phaser phaser = new Phaser(2);
+
+        new Washer(phaser);
+        new Washer(phaser);
+    }
+
+    static class Washer extends Thread {
+        Phaser phaser;
+
+        public Washer(Phaser phaser) {
+            this.phaser = phaser;
+            start();
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(getName() + "washing");
+                phaser.arriveAndAwaitAdvance();
+            }
+        }
+    }
+}
