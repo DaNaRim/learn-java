11b3fab1c973ba58ccd1bc2e1d9507a1157e4f10
DaNaRim <rangar.danarim@gmail.com>
2022-02-28 23:00:48 +0200

    multithreading lesson23

---
+++ src/multithreading/lesson23/Main.java
@@ -0,0 +1,24 @@
+package multithreading.lesson23;
+
+import java.util.concurrent.ThreadFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ThreadFactory tf = r -> {
+            Thread thread = new Thread(r);
+            thread.setPriority(Thread.MAX_PRIORITY);
+            return thread;
+        };
+
+        tf.newThread(new MyRun()).start();
+    }
+
+
+    static class MyRun implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(1);
+        }
+    }
+}
