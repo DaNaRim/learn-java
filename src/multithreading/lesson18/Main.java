d604e9313b4cea919ff8a472613fd501c6ea8fb2
DaNaRim <rangar.danarim@gmail.com>
2022-02-28 23:00:10 +0200

    multithreading lesson18

---
+++ src/multithreading/lesson18/Main.java
@@ -0,0 +1,40 @@
+package multithreading.lesson18;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch cdl = new CountDownLatch(3);
+
+//        cdl.countDown();
+//        cdl.await();
+
+        new Work(cdl);
+        new Work(cdl);
+        new Work(cdl);
+
+        cdl.await();
+        System.out.println("all jobs done");
+    }
+}
+
+class Work extends Thread {
+    CountDownLatch cdl;
+
+    public Work(CountDownLatch cdl) {
+        this.cdl = cdl;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(Math.round(Math.random() * 10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done work");
+        cdl.countDown();
+    }
+}
