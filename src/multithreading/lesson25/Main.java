526ca6c9f5784bad426279a1b79fc7908a89ed1a
DaNaRim <rangar.danarim@gmail.com>
2022-02-28 23:06:26 +0200

    multithreading lesson25

---
+++ src/multithreading/lesson25/Main.java
@@ -0,0 +1,15 @@
+package multithreading.lesson25;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Math.random());
+        System.out.println(ThreadLocalRandom.current().nextInt());
+
+//        Thread.sleep(TimeUnit.DAYS.toMillis(14));
+        System.out.println(TimeUnit.DAYS.toMillis(14));
+    }
+}
